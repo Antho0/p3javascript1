8b6950c8ecca4bc1efdcabe56c526d4330b1f942
root <root@Anthony.localdomain>
2018-10-10 10:35:48 +0200

    p3exe1js

---
+++ Assets/JS/p3exe3.js
@@ -0,0 +1,12 @@
+function changecolor(){
+  var vert = document.getElementById ('text');
+  vert.style.color ='green';
+}
+function changecolori(){
+  var rouge = document.getElementById ('text');
+  rouge.style.color ='red';
+}
+function changecolora(){
+  var bleu = document.getElementById ('text');
+  bleu.style.color ='blue';
+}
